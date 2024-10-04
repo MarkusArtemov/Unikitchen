0439dd12935f8bb7a5090a7ffe0ccf5ac8a389d9
MarkusArtemov <Martemov16@gmail.com>
2024-10-04 15:51:35 +0200

    Create spring boot app

---
+++ Server/unikitchen/src/main/java/com/dreamteam/unikitchen/UnikitchenApplication.java
@@ -0,0 +1,13 @@
+package com.dreamteam.unikitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnikitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnikitchenApplication.class, args);
+	}
+
+}
