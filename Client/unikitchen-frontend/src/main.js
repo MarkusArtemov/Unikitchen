a249397b843ed017f073e2c5334bba8d033e3f4d
MarkusArtemov <Martemov16@gmail.com>
2024-11-05 15:06:45 +0100

    Add config in backend and frontend

--- Client/unikitchen-frontend/src/main.js
+++ Client/unikitchen-frontend/src/main.js
@@ -1,8 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/style/styles.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/style/styles.css';
+import axios from 'axios';
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+axios.defaults.baseURL = 'http://localhost:8080';
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
--- Server/unikitchen/src/main/java/com/dreamteam/unikitchen/config/CorsConfig.java
+++ Server/unikitchen/src/main/java/com/dreamteam/unikitchen/config/CorsConfig.java
@@ -1,10 +1,9 @@
 package com.dreamteam.unikitchen.config;
 
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
@@ -13,10 +12,10 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:8082")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true)
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization")
@@ -25,5 +24,3 @@ public class CorsConfig {
         };
     }
 }
-
-
